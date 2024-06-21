2d7daed1a0a63140f6fdc47494431a8b4cc7c233
PinchoPel <noxam13@gmail.com>
2024-06-21 12:58:21 +0200

    guardar

--- src/componentes/main/knowledges/knowledges.js
+++ src/componentes/main/knowledges/knowledges.js
@@ -6,7 +6,7 @@ const logos = [
         nombre: "HTML5"
       },
       {
-        imagen: "public\images\css-3.svg",
+        imagen: "public/images/css-3.svg",
         nombre: "CSS3"
       },
       {
